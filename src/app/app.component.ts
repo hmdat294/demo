f9fe67ea9efc06ac830717857c5de71cd1fffab2
bin0931142237 <bin0931142237@gmail.com>
2024-07-23 21:38:55 +0700

    'thienangular'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IndexComponent } from './index/index.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,IndexComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'test';
+  
 }
